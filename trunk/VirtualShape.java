ba0d2d5e756bbb7a34a7f063eb597c397b4ec84f
Omer.Baror <Omer.Baror@5068cde2-5c28-0410-b1ca-e5c1fdeeaee5>
2007-04-01 23:42:50 +0000

    We made VirtualShape a class (extended by all virtual shapes) rather than an interface and moved all functions that all shapes share into it. We also added color and texture variables to all shapes. Finally, we created a new function nudgeRelative that will eventually nudge an element relative to its current position. We need Joel's help with facing to get it working. -- Benjamin and Omer.

--- trunk/VirtualShape.java
+++ trunk/VirtualShape.java
@@ -1,7 +1,78 @@
-public interface VirtualShape
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+
+public class VirtualShape
 {
-	public float[] getBoundingBox();
-	public void scale( double[] _factors );
-	public float[] getPosition();
-	public float[] getFacing();
+	private float[] position;
+	private float[] facing; //in quaternions
+	protected float[] boundingBox;
+	private int color;
+	private String texture;
+	
+	public VirtualShape(float[] _position)
+	{
+		position = _position;
+	}
+
+	public VirtualShape(Node _info)
+	{
+		Element info = (Element) _info;
+		NodeList colorNodes = info.getElementsByTagName("color");
+		if(colorNodes.getLength() != 0)
+		{
+			color = (int)Long.parseLong(colorNodes.item(0).getTextContent(),16);	
+		}
+
+		NodeList textureNodes = info.getElementsByTagName("texture");
+		if(textureNodes.getLength() != 0)
+		{
+			texture = textureNodes.item(0).getTextContent();	
+		}
+
+		NodeList positionNodes = info.getElementsByTagName("position");
+		position = new float[positionNodes.getLength()];
+		for(int i = position.length-1; i>=0; i--)
+		{
+			position[i] = Float.parseFloat(positionNodes.item(i).getTextContent());
+		}
+
+		NodeList facingNodes = info.getElementsByTagName("facing");
+		float[] rotEuler = new float[facingNodes.getLength()]; //construct an array of Euler facings
+		for(int i = rotEuler.length-1; i>=0; i--)
+		{
+			rotEuler[i] = Float.parseFloat(facingNodes.item(i).getTextContent());
+			rotEuler[i] = (float)Math.toRadians(rotEuler[i]); //change to Radians. We like radians.
+		}
+		if(rotEuler.length == 3)
+			facing = Quaternions.getQuatFromEuler(rotEuler); //set the facing to be in Quaternions
+		else
+			facing = Constants.DEFAULT_FACING; //set to a unit
+	}
+
+	public float[] getPosition()
+	{
+		return position;
+	}
+	
+	public float[] getFacing()
+	{
+		return facing;
+	}
+
+	public float[] getBoundingBox()
+	{
+		return boundingBox;
+	}
+
+	public int getColor()
+	{
+		return color;
+	}
+
+	public String getTexture()
+	{
+		return texture;
+	}
+
+	public void scale( double[] _factors ){ }
 }
--- trunk/VirtualSphere.java
+++ trunk/VirtualSphere.java
@@ -1,44 +1,24 @@
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
 
-public class VirtualSphere implements VirtualShape
+public class VirtualSphere extends VirtualShape
 {
 	private float radius;
-	private float[] position;
-	private float[] facing; //in quaternions
-	private float[] boundingBox;
 
 	public VirtualSphere(Node _info)
 	{
+		super(_info);
 		Element info = (Element) _info;
 		Element _radius = (Element) info.getElementsByTagName("radius").item(0);
 		radius = Float.parseFloat(_radius.getTextContent());
-		NodeList positionNodes = info.getElementsByTagName("position");
-		position = new float[positionNodes.getLength()];
-		for(int i = position.length-1; i>=0; i--)
-		{
-			position[i] = Float.parseFloat(positionNodes.item(i).getTextContent());
-		}
-	
-		NodeList facingNodes = info.getElementsByTagName("facing");
-		float[] rotEuler = new float[facingNodes.getLength()]; //construct an array of Euler facings
-		for(int i = rotEuler.length-1; i>=0; i--)
-		{
-			rotEuler[i] = Float.parseFloat(facingNodes.item(i).getTextContent());
-			rotEuler[i] = (float)Math.toRadians(rotEuler[i]); //change to Radians. We like radians.
-		}
-		if(rotEuler.length == 3)
-			facing = Quaternions.getQuatFromEuler(rotEuler); //set the facing to be in Quaternions
-		else
-			facing = Constants.DEFAULT_FACING; //set to a unit
 
 		boundingBox = new float[] {radius, radius, radius};
 	}
 
 	public VirtualSphere(float _radius, float[] _position)
 	{
+		super(_position);
 		radius = _radius;
-		position = _position;
 		boundingBox = new float[] {radius, radius, radius};
 	}
 
@@ -46,22 +26,6 @@ public class VirtualSphere implements VirtualShape
 	{
 		return radius;
 	}
-
-	public float[] getPosition()
-	{
-		return position;
-	}
-	
-	public float[] getFacing()
-	{
-		return facing;
-	}
-
-	public float[] getBoundingBox()
-	{
-		return boundingBox;
-	}
-
 
 	public void scale( double[] factors )
 	{
