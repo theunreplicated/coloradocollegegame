5f0bd9c3b64d342a49cf35584bed66fac90e3058
Omer.Baror <Omer.Baror@5068cde2-5c28-0410-b1ca-e5c1fdeeaee5>
2007-05-14 00:52:29 +0000

    I optimized a bit of the Resolver and renamed some variables to make it easier to understand. I'm not sure that ActionsHashMap is ever used now. We should consider getting rid of it, but for now it doesn't really hurt.
    Also: lots of new comments! Check out Resolver.java and RepresentationResolver.java!

--- RepresentationResolver.java
+++ RepresentationResolver.java
@@ -1,10 +1,27 @@
 import javax.script.*;
+
+/**
+ * The RepresentationResolver class handles the "inrepresentation"
+ * component of actions. It simply calls them and gives them access
+ * to the Representation.
+ * @author Omer Bar-or
+ * @version 1.0, May 2007
+ */
 public class RepresentationResolver
 {
+	// global variables
 	private Representation rep;
 	private ScriptEngineManager manager;
 	private Logger myLogger;
 
+	/**
+	 * The constructor for RepresentationResolver. It creates the
+	 * ScriptEngineManager that will create all of the engines (each
+	 * script gets its own engine currently) and passes all of the
+	 * variables to the manager that all scripts will need.
+	 * @param	_rep	the Representation that gets passed to the scripts
+	 * @param _myLogger the Logger (also passed to scripts)
+	 */
 	public RepresentationResolver(Representation _rep, Logger _myLogger)
 	{
 		rep = _rep;
@@ -19,13 +36,26 @@ public class RepresentationResolver
 		manager.put("VectorUtils", new VectorUtils());
 	}
 
+	/* Ignore the fallthrough of our switch statement in the following
+	 * function.
+	 */
 	@SuppressWarnings("fallthrough")
+
+	/**
+	 * Resolves the "inrepresentation" component of a particular Action.
+	 * @param _action the Action to resolve
+	 * @see ScriptEngine
+	 */
 	public void resolve(Action _action)
 	{
 		GameElement[] nouns = _action.getNouns();
 		GameElement subject = null, directObject = null, indirectObject = null;
 		GameElement[] other = null;
 		nouns = _action.getNouns();
+
+		/* For an explanation of this statement, see the same code in
+		 * Resolver.java
+		 */
 		if(nouns != null)
 		{
 			switch(nouns.length)
@@ -43,6 +73,7 @@ public class RepresentationResolver
 			}
 		}
 
+		// call the representation function ("inrepresentation") of the Action.
 		StringFunction repFunc = _action.getRepFunction();
 		try {
 			ScriptEngine engine = manager.getEngineByName(repFunc.getLanguage());
@@ -53,12 +84,15 @@ public class RepresentationResolver
 			engine.put("argv",_action.parameters().pack());
 			engine.eval(repFunc.getFunction());
 		}
+		/* All scripts throw rather generic exceptions called ScriptExceptions.
+		 * Here, we catch it and print the Action that caused it.
+		 */
 		catch(ScriptException se)
 		{
-			myLogger.message("Script error: " + se.getMessage() + "\n",true);
-
-			System.out.println("error in RepResolver");
-			System.out.println(_action);
+			myLogger.message("Script error (in RepResolver): " + se.getMessage() + "\n",true);
+			myLogger.message(_action.toString() + "\n",true);
 		}
-	}
-}
+
+	} // public void resolve
+
+} // public class RepresentationResolver
